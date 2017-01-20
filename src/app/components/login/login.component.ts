2c449fbaaf9cd3d432aeaf91e2a438e912ae7444
Bartosz <bartek.g55@gmail.com>
2017-01-20 23:06:48 +0000

    Added router redirection after succesful login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'login',
@@ -9,11 +10,21 @@ import { UserService } from '../../services/user.service';
 export class LoginComponent implements OnInit {
     private status: string;
     
-    constructor(private userService: UserService) { }
+    constructor(
+        private userService: UserService,
+        private router: Router
+        ) { }
 
     ngOnInit() { }
 
     logIn(email: string, password: string): void {
-        this.userService.logIn(email, password).subscribe(status => localStorage.setItem('status', status['status']));
+        this.userService.logIn(email, password).subscribe(status => {
+            sessionStorage.setItem('status', status['status']);
+            if(sessionStorage.getItem('status') == 'logged') {
+                this.router.navigate(['/categories']);
+            } else {
+                alert('Wrong email or password');
+            }
+        });
     }
 }
